a146eb0324c2ec85f399a467dbff65f166f65287
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-19 14:01:12 +0200

    First commit | Bot deploy

---
+++ index.js
@@ -0,0 +1,36 @@
+//const { Telegraf, Telegram } = require('telegraf');
+const { Composer } = require('micro-bot');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const bot = new Composer();
+
+
+bot.command('start', (ctx) => {
+    const user = ctx.message.chat.first_name;
+    ctx.telegram.sendMessage(ctx.chat.id, `Benvenuto ${user}, seleziona quello che ti serve`,
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: "Programma", callback_data: "programma"}, {text: "Informazioni", callback_data: "informazioni"}]
+            ]
+        }
+    });
+});
+
+bot.action('programma', (ctx) => {
+    ctx.deleteMessage();
+    ctx.reply("Ecco il link del programma\nhttps://docs.google.com/spreadsheets/d/1kOQY13lzyX8CF9iBRnAZGr8Xz3hUtca6PmQV0_ZUsfE/edit#gid=537086263");
+});
+bot.action('informazioni', (ctx) => {
+    ctx.deleteMessage();
+    ctx.reply("Bot creato in Nodejs da Alessandro Fogli Iseppe, utilizzando il framework telegraf.js [Deployed on Heroku]");
+})
+
+//bot.launch();
+module.exports = bot;
+
+//appId = hidden-shore-42294
+//herokuURL = https://hidden-shore-42294.herokuapp.com/
+//TOKEN = '5302564968:AAEr9BsXwKF880Di7oKDPXV5f3E8_Xt4jMg'
