f7a87396488795b731aeb2672ea26d07eda17f97
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-28 12:24:22 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-//npm i telegraf --save
+//telegraf
 //const { Telegraf, Telegram } = require('telegraf');
+//micro-bot
 const { Composer } = require('micro-bot');
 const dotenv = require('dotenv');
 
 dotenv.config();
+//telegraf
 //const bot = new Telegraf(process.env.TOKEN);
+//micro-bot
 const bot = new Composer();
 
 
 bot.command('start', (ctx) => {
     const user = ctx.message.chat.first_name;
+    
     ctx.telegram.sendMessage(ctx.chat.id, `Benvenuto ${user}, seleziona quello che ti serve`,
     {
         reply_markup: {
@@ -27,8 +31,10 @@ bot.action('programma', (ctx) => {
 bot.action('informazioni', (ctx) => {
     ctx.deleteMessage();
     ctx.reply("Bot creato in Nodejs da Alessandro Fogli Iseppe, utilizzando il framework telegraf.js [Deployed on Heroku]");
-})
+});
 
+//telegraf
 //bot.launch();
+//micro-bot
 module.exports = bot;
 
